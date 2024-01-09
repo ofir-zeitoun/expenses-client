7279ebcda63d36471c47d0167997e9874ce16d33
riki <riki.sh700@gmail.com>
2024-01-09 16:30:17 +0200

    add querywrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import mernLogo from "./assets/oz-mern.png";
 import ozLogo from "/oz.png";
 import "./App.css";
+import Expenses from "./components/expenses/expenses";
+import Header from './components/header/header';
+import { QueryWrapper } from "./components/utilities/query-wrapper";
+
 
 function App() {
+
   return (
     <>
+    <QueryWrapper>
       <div>
+      <Header/>
+        <Expenses/>
         <img src={ozLogo} className="logo" alt="Ofir Zeitoun" />
         <img src={mernLogo} className="logo react" alt="MERN" />
       </div>
+      </QueryWrapper>
     </>
   );
 }
