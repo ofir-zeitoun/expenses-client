c0c17eb4e1010b8f238bf1db019bc249bfb96e6c
RikiAmar <94980711+RikiAmar@users.noreply.github.com>
2024-01-10 15:16:04 +0200

    Feature 3/riki/get all expanses react query (#13)
    * add react query
    
    * ignore .env file
    
    * add querywrapper
    
    * add expenses

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2 className="title">Expenses</h2>
+      <button className="btn-Add">Add</button>
+    </div>
+  )
+}
+
+export default Header
