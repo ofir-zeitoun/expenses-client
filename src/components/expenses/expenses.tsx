907e2f35a1ca10c5512666fd6bf2660cf8be26e4
Ofir <ofir.zeitoun@gmail.com>
2024-01-12 10:37:03 +0200

    refactor base api

--- src/components/expenses/expenses.tsx
+++ src/components/expenses/expenses.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import fetchExpenses from "../../api/expensesApi";
+import fetchExpenses from "./fetch-expenses";
 
 const Expenses = () => {
   const { data: expenses } = useQuery({
