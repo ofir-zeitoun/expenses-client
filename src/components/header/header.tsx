fe699639f585ef70b135c5b8e021baa8e842e821
Ofir <ofir.zeitoun@gmail.com>
2024-01-12 10:38:45 +0200

    add index to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,8 @@
-
-
-const Header = () => {
+export const Header = () => {
   return (
     <div className="header">
       <h2 className="title">Expenses</h2>
       <button className="btn-Add">Add</button>
     </div>
-  )
-}
-
-export default Header
+  );
+};
