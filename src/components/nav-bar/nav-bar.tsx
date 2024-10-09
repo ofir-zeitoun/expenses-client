7941979142a7c7a895d449181016e6a9833484fa
Lior Silman <lior.silmansoad@gmail.com>
2024-10-09 23:54:14 +0300

    Create `modals/settings-modal.tsx` component in new `modals` directory Add `isSettingVisible` state in `NavBar` component for conditional rendering of `SettingsModal` Include `onOk` and `onCancel` props for `SettingsModal` to handle visibility toggle

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -11,10 +11,14 @@ import { ThemeSwitcher } from "../theme-switcher";
 import "./nav-bar.css";
 import { TranslationSwitcher } from "../translation";
 import { useTranslation } from "react-i18next";
+import { SettingsModal } from "../modals/settings-modal";
+import { useState } from "react";
+import { is } from "date-fns/locale";
 
 export const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-  const {t} = useTranslation();
+  const [isSettingSelected, setIsSettingSelected] = useState(false);
+  const { t } = useTranslation();
   const items = [
     {
       key: "profile",
@@ -25,11 +29,15 @@ export const NavBar = () => {
       key: "settings",
       icon: <SettingOutlined />,
       label: t("settings"),
+      onClick: () => {
+        console.log("settings clicked");
+        setIsSettingSelected(true);
+      },
     },
     {
       key: "logout",
       icon: <LogoutOutlined />,
-      label:t("logout"),
+      label: t("logout"),
       className: "menu-logout",
       onClick: () =>
         logout({ logoutParams: { returnTo: window.location.origin } }),
@@ -37,56 +45,68 @@ export const NavBar = () => {
   ];
 
   return (
-    <div className="navbar">
-      <div className="left-nav">
-        <img src="/src/assets/oz-mern.png" alt="Logo" className="logo" />
-        <span className="app-name">{t("app-name")}</span>
-      </div>
-      <ThemeSwitcher />
-      <TranslationSwitcher />
-      <div className="right-nav">
-        <div className="connection-wrapper">
-          <ConnectionHealth />
+    <>
+      <div className="navbar">
+        <div className="left-nav">
+          <img src="/src/assets/oz-mern.png" alt="Logo" className="logo" />
+          <span className="app-name">{t("app-name")}</span>
         </div>
-        <div className="auth-container">
-          {isAuthenticated ? (
-            <>
-              <div className="user-photo">
-                <Avatar
-                  src={user?.picture}
-                  icon={<UserOutlined />}
-                  alt="User"
-                />
-              </div>
-              <div className="user-name">{user?.name || "Unknown"}</div>
-              <div className="user-menu">
+        <ThemeSwitcher />
+        <TranslationSwitcher />
+        <div className="right-nav">
+          <div className="connection-wrapper">
+            <ConnectionHealth />
+          </div>
+          <div className="auth-container">
+            {isAuthenticated ? (
+              <>
+                <div className="user-photo">
+                  <Avatar
+                    src={user?.picture}
+                    icon={<UserOutlined />}
+                    alt="User"
+                  />
+                </div>
+                <div className="user-name">{user?.name || "Unknown"}</div>
+                <div className="user-menu">
+                  <Dropdown
+                    menu={{ items }}
+                    placement="bottomRight"
+                    trigger={["click"]}
+                  >
+                    <MoreOutlined />
+                  </Dropdown>
+                </div>
+              </>
+            ) : (
+              <>
+                <span
+                  className="user-login"
+                  onClick={() => loginWithRedirect()}
+                >
+                  {t("login")}
+                </span>
                 <Dropdown
-                  menu={{ items }}
+                  menu={{
+                    items: items.filter((item) => item.key === "settings"),
+                  }}
                   placement="bottomRight"
                   trigger={["click"]}
                 >
                   <MoreOutlined />
                 </Dropdown>
-              </div>
-            </>
-          ) : (
-            <>
-              <span className="user-login" onClick={() => loginWithRedirect()}>
-                {t("login")}
-              </span>
-              <Dropdown
-                menu={{
-                  items: items.filter((item) => item.key === "settings"),
-                }}
-                placement="bottomRight"
-                trigger={["click"]}
-              >
-                <MoreOutlined />
-              </Dropdown>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      {isSettingSelected && (
+        <SettingsModal
+          onOk={() => setIsSettingSelected(false)}
+          onCancel={() => setIsSettingSelected(false)}
+          visible={isSettingSelected}
+        />
+      )}
+    </>
   );
 };
