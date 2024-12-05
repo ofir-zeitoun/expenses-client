f9f18cf7ece2ddb5a761ed7932c95e85b28a7464
Shaked Levy <shakedlv0@gmail.com>
2024-12-05 19:20:32 +0200

    refactor hooks

--- src/components/translation/translation.tsx
+++ src/components/translation/translation.tsx
@@ -1,15 +1,13 @@
 import { useMemo } from "react";
-import { Dropdown } from "antd";
 import { changeLanguage } from 'i18next';
+import { Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
+import { FlagIcon } from "../flag-icon";
 import { LanguageOption } from "./language-option";
 import './translation.css';
-import { useTextDirection } from '../utilities/useTextDirection';
-import { FlagIcon } from "../flag-icon";
 
 
 export const TranslationSwitcher = () => {
-    const changeDirWithLanguage = useTextDirection();
     const languages = useMemo(() => {
 
         return LanguageOption.map(lang => {
@@ -19,7 +17,6 @@ export const TranslationSwitcher = () => {
                 label: lang.label,
                 onClick: () => {
                     changeLanguage(lang.key);
-                    changeDirWithLanguage(lang.key);
                 },
 
             })
