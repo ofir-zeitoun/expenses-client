55970e715476bc9b7685bb2da218c3d713d31575
Lior Silman <lior.silmansoad@gmail.com>
2024-12-14 16:25:17 +0200

    Refactor App component to import Dashboard and NavBar earlier and remove unused translation service

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Dashboard, NavBar } from "./components";
 import { QueryWrapper, useTheme } from "./components/utilities";
+import { Dashboard, NavBar } from "./components";
 import "./App.css";
-import i18n from "./services/translation";
 
 function App() {
   const { isAuthenticated } = useAuth0();
   useTheme();
-  console.log("i18n-direction", i18n.dir());
 
   return (
     <QueryWrapper>
